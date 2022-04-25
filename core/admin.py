a1b011e66fa19ee7b824ab00ca75deb6cdd13dac
Murad Aghazada <tariyelagazade@Murads-MacBook-Pro3.local>
2022-04-25 20:29:45 +0400

    m

--- core/admin.py
+++ core/admin.py
@@ -1,11 +1,12 @@
+from curses.ascii import EM
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, Email, Number
 
-admin.site.register([Category,])
+admin.site.register([Category, Email, Number,])
 
 class ProductAdmin(admin.ModelAdmin):
     model = Product
-    list_display = ['id','title', 'category', 'size', 'price', 'support_price', 'image',] 
+    list_display = ['id','title', 'category', 'code', 'address', 'size', 'retailer', 'retail_price', 'price', 'support_price', 'image',] 
     list_display_links = ('id',)
     list_editable = ['title', 'category', 'size', 'price', 'support_price', 'image',] 
 
